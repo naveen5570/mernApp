ed709f564fc4e622089ceb8a25ceb354fe6b1827
instrovate <lms@instrovate.com>
2022-11-29 17:16:54 +0530

    upload

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const services_fr = require('./routes/api/fr/Services');
 
 
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+
 app.use(bodyParser.json());
 
 app.use('/api/tasks', routeTasks, (req, res) => res.sendStatus(401));
@@ -62,8 +62,12 @@ app.use('/api/fr/pages', pages_fr);
 app.use('/api/fr/services', services_fr);
 
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+
+app.use(express.static('./client/build'));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build",     
+  "index.html"));
 });
 
 
@@ -71,7 +75,6 @@ app.get('*', (req, res) => {
 
 
 
-const port = process.env.PORT || 5000;
-app.listen(port);
+app.set('port', process.env.PORT || 5000);
 
 console.log(`listening on ${port}`);
