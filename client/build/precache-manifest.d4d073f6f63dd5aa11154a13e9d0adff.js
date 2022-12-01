5d3b5ae40a8728652a94cec1e4ef5e3989eb913c
naveen5570 <lms@instrovate.com>
2022-12-01 13:52:10 +0530

    build folder

---
+++ client/build/precache-manifest.d4d073f6f63dd5aa11154a13e9d0adff.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "bfdcf2060cf1919d55fb",
+    "url": "/static/css/main.0028c923.chunk.css"
+  },
+  {
+    "revision": "bfdcf2060cf1919d55fb",
+    "url": "/static/js/main.bfdcf206.chunk.js"
+  },
+  {
+    "revision": "bfd9ed9526c57a39a3aa",
+    "url": "/static/css/1.30731a87.chunk.css"
+  },
+  {
+    "revision": "bfd9ed9526c57a39a3aa",
+    "url": "/static/js/1.bfd9ed95.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d45be21899879ada54d3be4b5a4cb20b",
+    "url": "/static/media/banner.d45be218.jpg"
+  },
+  {
+    "revision": "1ba5558ce2b11631185ff9e9fc3a206d",
+    "url": "/static/media/footer-bg.1ba5558c.png"
+  },
+  {
+    "revision": "8792ba43aace3b23fd8e179997747fe8",
+    "url": "/static/media/header-img-2.8792ba43.jpg"
+  },
+  {
+    "revision": "39e2fb3f7f3eaa56c270dc4ea018563d",
+    "url": "/static/media/header-img.39e2fb3f.jpg"
+  },
+  {
+    "revision": "a230989eb66bcacfefa1b48495a33737",
+    "url": "/index.html"
+  }
+];
